6e0ad996e95cc0bf90b0e08139f9dd6b50c2faef
liyuhao <liyuhaoid@sina.com>
2021-08-11 10:40:41 +0800

    一个可用demo

--- moudle/src/main/java/spa/lyh/cn/moudle/BaseActivity.java
+++ moudle/src/main/java/spa/lyh/cn/moudle/BaseActivity.java
@@ -4,7 +4,7 @@ import android.content.Context;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import spa.lyh.cn.languagepack.LanguagesPack;
+import spa.lyh.cn.moudle.utils.LanguageUtils;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -12,7 +12,10 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void attachBaseContext(Context newBase) {
         // 国际化适配（绑定语种）
-        super.attachBaseContext(LanguagesPack.attach(newBase));
-        //super.attachBaseContext(newBase);
+        if (LanguageUtils.isActivited()){
+            super.attachBaseContext(LanguageUtils.getLanguageContext(newBase));
+        }else {
+            super.attachBaseContext(newBase);
+        }
     }
 }
--- moudle/src/main/java/spa/lyh/cn/moudle/utils/LanguageUtils.java
+++ moudle/src/main/java/spa/lyh/cn/moudle/utils/LanguageUtils.java
@@ -1,5 +1,34 @@
 package spa.lyh.cn.moudle.utils;
 
-class LanguageUtils {
+import android.content.Context;
+
+import java.lang.reflect.Method;
+
+public class LanguageUtils {
     private final static String className = "spa.lyh.cn.languagepack.LanguagesPack";
+
+    /**
+     * 检查分享模块是否启动
+     * @return
+     */
+    public static boolean isActivited(){
+        try{
+            Class.forName(className);
+            return true;
+        }catch (Exception ignored){
+            return false;
+        }
+    }
+
+    public static Context getLanguageContext(Context context){
+        Object obj;
+        try{
+            Class clazz = Class.forName(className);
+            Method method = clazz.getMethod("attach",Context.class);
+            obj = method.invoke(clazz,context);
+            return (Context) obj;
+        }catch (Exception ignored){
+            return null;
+        }
+    }
 }
